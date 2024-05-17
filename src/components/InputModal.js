0599a7ef6e1eab34fed18a53d561e6953e6a0ada
imytdotio <imytdotio@gmail.com>
2024-05-18 01:20:02 +0800

    better image uploader

--- src/components/InputModal.js
+++ src/components/InputModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { FaImage } from 'react-icons/fa';
 
 const InputModal = ({ onAddContent, onClose }) => {
   const [content, setContent] = useState('');
@@ -6,7 +7,6 @@ const InputModal = ({ onAddContent, onClose }) => {
   const [isUploading, setIsUploading] = useState(false);
   const modalRef = useRef(null);
   const imgbbApiKeyLink = 'https://api.imgbb.com/1/upload?key=' + process.env.REACT_APP_IMGBB_API_KEY;
-
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
@@ -38,11 +38,6 @@ const InputModal = ({ onAddContent, onClose }) => {
     }
   };
 
-  const toggleInputType = () => {
-    setIsImage(!isImage);
-    setContent('');
-  };
-
   const handleFileChange = async (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -57,6 +52,7 @@ const InputModal = ({ onAddContent, onClose }) => {
         const result = await response.json();
         if (result.success) {
           setContent(result.data.url);
+          setIsImage(true);
         } else {
           alert('Image upload failed');
         }
@@ -72,46 +68,43 @@ const InputModal = ({ onAddContent, onClose }) => {
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
       <div ref={modalRef} className="bg-white p-2 rounded-3xl w-96">
-        <button
-          className="w-full bg-gray-300 text-black p-2 mb-2 rounded-3xl"
-          onClick={toggleInputType}
-        >
-          {isImage ? 'Switch to Text' : 'Switch to Image URL'}
-        </button>
-        {isImage ? (
+        {!isImage && !isUploading && (
           <>
-            <input
-              type="file"
-              accept="image/*"
-              className="w-full p-2 border rounded-3xl mb-2"
-              onChange={handleFileChange}
-              disabled={isUploading}
-            />
-            {isUploading && <p className="text-center">Uploading...</p>}
-            {content && !isUploading && (
-              <button
-                className="bg-black text-white p-2 w-full rounded-3xl mt-2"
-                onClick={handleSubmit}
-              >
-                Upload
-              </button>
-            )}
-          </>
-        ) : (
-          <>
+            <div className="h-60 bg-gray-300 rounded-3xl border border-dashed border-black flex flex-col justify-center items-center cursor-pointer mb-2"
+              onClick={() => document.getElementById('fileInput').click()}
+            >
+              <FaImage className="text-white text-5xl" />
+              <p className="text-white mt-2">Upload Image</p>
+            </div>
             <textarea
               className="w-full p-2 border rounded-3xl mb-2"
               placeholder="Input any text..."
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
-            <button
-              className="bg-black text-white p-2 w-full rounded-3xl"
-              onClick={handleSubmit}
-            >
-              Upload
-            </button>
           </>
+        )}
+        {isUploading && <p className="text-center mb-2">Uploading...</p>}
+        {isImage && !isUploading && (
+          <div className="h-60">
+            <img src={content} alt="Uploaded" className="w-full h-full object-cover rounded-3xl mb-2" />
+          </div>
+        )}
+        <input
+          id="fileInput"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={handleFileChange}
+          disabled={isUploading}
+        />
+        {(content && !isUploading) && (
+          <button
+            className="bg-black text-white p-2 w-full rounded-3xl mt-2"
+            onClick={handleSubmit}
+          >
+            Upload
+          </button>
         )}
       </div>
     </div>
