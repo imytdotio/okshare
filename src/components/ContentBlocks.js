55dfc3bafdeccf9bdb98a570afe4024f983c2723
imytdotio <imytdotio@gmail.com>
2024-05-18 01:26:41 +0800

    image download

--- src/components/ContentBlocks.js
+++ src/components/ContentBlocks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCopy } from 'react-icons/fa';
+import { FaCopy, FaDownload } from 'react-icons/fa';
 
 const ContentBlocks = ({ contents, onAddClick }) => {
   const handleTextClick = (text) => {
@@ -10,13 +10,22 @@ const ContentBlocks = ({ contents, onAddClick }) => {
     });
   };
 
+  const handleImageClick = (url) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'image';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="grid grid-cols-2 gap-2">
       {contents.map((item, index) => (
         <div
           key={index}
-          className={`relative bg-white ${item.type === 1 ? 'p-0' : 'p-4 hover:cursor-pointer'} rounded-3xl h-40 shadow flex hover:bg-gray-300 transition-all duration-200`}
-          onClick={() => item.type === 0 && handleTextClick(item.content)}
+          className={`relative bg-white ${item.type === 1 ? 'p-0 hover:cursor-pointer' : 'p-4 hover:cursor-pointer'} rounded-3xl h-40 shadow flex transition-all duration-200`}
+          onClick={() => item.type === 0 ? handleTextClick(item.content) : handleImageClick(item.content)}
         >
           {item.type === 1 ? (
             <img src={item.content} alt={`Content ${index}`} className="w-full h-full object-cover rounded-3xl" />
@@ -25,11 +34,14 @@ const ContentBlocks = ({ contents, onAddClick }) => {
               {item.content}
             </p>
           )}
-          {item.type === 0 && (
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200">
-              <FaCopy className="text-white text-2xl" />
-            </div>
-          )}
+          <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200">
+            <div className="absolute inset-0 bg-black opacity-40 rounded-3xl"></div>
+            {item.type === 0 ? (
+              <FaCopy className="text-white text-2xl z-10" />
+            ) : (
+              <FaDownload className="text-white text-2xl z-10" />
+            )}
+          </div>
         </div>
       ))}
       <button
